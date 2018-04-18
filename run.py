883b9b6f4dd360c2fa96df6c47f2808888c715c8
yjqiang <13307130285@fudan.edu.cn>
2018-04-19 03:48:06 +0800

    格式化

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from Tasks import Tasks
 from connect import connect
 from rafflehandler import Rafflehandler
 import asyncio
-import utils
 from printer import Printer
 from statistics import Statistics
 from bilibili import bilibili
@@ -15,20 +14,16 @@ import os
 import biliconsole
 
 
-
-loop = asyncio.get_event_loop() 
+loop = asyncio.get_event_loop()
 fileDir = os.path.dirname(os.path.realpath('__file__'))
 file_color = fileDir + "/conf/color.conf"
 file_user = fileDir + "/conf/user.conf"
 file_bilibili = fileDir + "/conf/bilibili.conf"
-ConfigLoader(colorfile = file_color, userfile = file_user, bilibilifile = file_bilibili)
+ConfigLoader(colorfile=file_color, userfile=file_user, bilibilifile=file_bilibili)
 
 # print('Hello world.')
 printer = Printer()
 bilibili()
-
-# print('ok')
-
 Statistics()
 
 rafflehandler = Rafflehandler()
@@ -45,13 +40,13 @@ console_thread = threading.Thread(target=biliconsole.controler)
 
 console_thread.start()
 
-loop = asyncio.get_event_loop() 
+loop = asyncio.get_event_loop()
 tasks = [
-    utils.fetch_user_info(),
-    utils.fetch_bag_list(),
-    utils.fetch_medal(),
+    # utils.fetch_user_info(),
+    # utils.fetch_bag_list(),
+    # utils.fetch_medal(),
 
-    task.run(), 
+    task.run(),
     task1.run(),
     task2.run(),
     task4.connect(),
@@ -70,14 +65,6 @@ except KeyboardInterrupt:
         print('登出失败', response)
     else:
         print('成功退出登陆')
-    # loop.run_until_complete(asyncio.wait([bilibili().bili_section.close()]))
-    # task4.danmuji.close_connection()
-    # for task in tasks:
-     #   task.cancel()
-    # loop.run_forever()
-    # biliconsole.Biliconsole().terminite()
-    
-    
     
 console_thread.join()
 
