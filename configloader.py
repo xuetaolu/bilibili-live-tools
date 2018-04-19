f68600240fe77c8449eb1a9b83650f06d7b12d6a
yjqiang <13307130285@fudan.edu.cn>
2018-04-19 13:32:36 +0800

    加了一个小的防手贱true True这种，代码格式调整顺手

--- configloader.py
+++ configloader.py
@@ -2,6 +2,7 @@ import configparser
 import webcolors
 import codecs
 
+
 # "#969696"
 def hex_to_rgb_percent(hex_str):
     color = webcolors.hex_to_rgb_percent(hex_str)
@@ -16,9 +17,6 @@ def rgb_to_percent(rgb_str):
     return [float(i.strip('%'))/100.0 for i in color]
 
     
-
-    
-        
 def load_bilibili(file):
     cf_bilibili = configparser.ConfigParser()
     cf_bilibili.optionxform = str
@@ -26,7 +24,6 @@ def load_bilibili(file):
     cf_bilibili.read_file(codecs.open(file, "r", "utf8"))
     dic_bilibili = cf_bilibili._sections
     
-                
     dic_nomalised_bilibili = dic_bilibili['normal'].copy()
     
     dic_nomalised_bilibili['account'] = dic_bilibili['account'].copy()
@@ -41,32 +38,20 @@ def load_bilibili(file):
         dic_nomalised_bilibili['account']['username'] = username
         dic_nomalised_bilibili['account']['password'] = password
         
-    
-        
     dic_bilibili_type = dic_bilibili['types']
     # str to int
     for i in dic_bilibili_type['int'].split():
         
         dic_nomalised_bilibili[i] = int(dic_bilibili['normal'][i])
             
-    # str to bool
-    # for i in dic_bilibili_type['bool'].split():
-    
-        # dic_nomalised_bilibili[i] = True if dic_bilibili['normal'][i] == 'True' else False
-
-               
-        
-        # str to dic
     for i in dic_bilibili.keys():
         # print(i)
         if i[0:3] == 'dic':
             dic_nomalised_bilibili[i[4:]] = dic_bilibili[i]
             
-    
-                
-    
     return dic_nomalised_bilibili
-        
+
+                
 def load_color(file):
     cf_color = configparser.ConfigParser()
     
@@ -81,14 +66,17 @@ def load_color(file):
                 i[j] = rgb_to_percent(i[j])
                     
     return dic_color
-        
+ 
+               
 def load_user(file):
     cf_user = configparser.ConfigParser()
     cf_user.read_file(codecs.open(file, "r", "utf8"))
     dic_user = cf_user._sections
-    dictionary ={
-            'True':True, 
+    dictionary = {
+            'True': True,
             'False': False,
+            'true': True,
+            'false': False,
             'user': 0,
             'debug': 1
         }
@@ -107,14 +95,14 @@ class ConfigLoader():
     
     instance = None
 
-    def __new__(cls, colorfile = None, userfile = None, bilibilifile = None):
+    def __new__(cls, colorfile=None, userfile=None, bilibilifile=None):
         if not cls.instance:
             cls.instance = super(ConfigLoader, cls).__new__(cls)
             cls.instance.dic_color = load_color(colorfile)
             # print(self.dic_color)
         
             cls.instance.dic_user = load_user(userfile)
-            #print(self.dic_user)
+            # print(self.dic_user)
         
             cls.instance.dic_bilibili = load_bilibili(bilibilifile)
             # print(self.dic_bilibili)
